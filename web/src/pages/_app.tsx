572aea2e67ba15b319e52886654427f3f07235c3
henriqueholz <henriqueholz@live.com>
2022-05-15 21:28:05 -0300

    Finishing frondend

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,10 +1,12 @@
-
-import { UserProvider } from "@auth0/nextjs-auth0"
+import { UserProvider } from '@auth0/nextjs-auth0'
+import '../styles/global.css';
 
 function MyApp({ Component, pageProps }) {
-  <UserProvider>
-    return <Component {...pageProps} />
-  </UserProvider>
+  return (
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+  )
 }
 
 export default MyApp
